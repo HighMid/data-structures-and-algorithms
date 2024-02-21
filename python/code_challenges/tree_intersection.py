b35206ea94081b75e4b26be5e0c176672bfa85b4
HighMid <xstar671@gmail.com>
2024-02-20 23:22:00 -0500

    Completed Challenge32

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,25 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
+def tree_intersection(tree_a, tree_b):
 
-def tree_intersection():
-    pass
+    def traverse_and_store(node, values_set):
+        if node:
+            values_set.add(node.value)
+            traverse_and_store(node.left, values_set)
+            traverse_and_store(node.right, values_set)
+    
+    values_set_a = set()
+    traverse_and_store(tree_a.root, values_set_a)
+
+    common_values = set()
+    
+    def collect_common_values(node, values_set, common_values):
+        if node:
+            if node.value in values_set:
+                common_values.add(node.value)
+            collect_common_values(node.left, values_set, common_values)
+            collect_common_values(node.right, values_set, common_values)
+    
+    collect_common_values(tree_b.root, values_set_a, common_values)
+    
+    return list(common_values)
--- python/tests/code_challenges/test_tree_intersection.py
+++ python/tests/code_challenges/test_tree_intersection.py
@@ -7,8 +7,6 @@ from data_structures.queue import Queue
 def test_exists():
     assert tree_intersection
 
-
-@pytest.mark.skip("TODO")
 def test_tree_intersection():
 
     tree_a = BinaryTree()
@@ -40,3 +38,47 @@ def add_values_to_empty_tree(tree, values):
         node.right = Node(values.pop()) if values else None
         q.enqueue(node.left)
         q.enqueue(node.right)
+
+def test_no_common_elements():
+    tree_a = BinaryTree()
+    add_values_to_empty_tree(tree_a, [1, 2, 3, 4, 5])
+
+    tree_b = BinaryTree()
+    add_values_to_empty_tree(tree_b, [6, 7, 8, 9, 10])
+
+    assert tree_intersection(tree_a, tree_b) == []
+
+def test_all_elements_common():
+    tree_a = BinaryTree()
+    add_values_to_empty_tree(tree_a, [11, 7, 15, 5, 9, 13, 17])
+
+    tree_b = BinaryTree()
+    add_values_to_empty_tree(tree_b, [11, 7, 15, 5, 9, 13, 17])
+
+    assert sorted(tree_intersection(tree_a, tree_b)) == sorted([11, 7, 15, 5, 9, 13, 17])
+
+def test_one_tree_empty():
+    tree_a = BinaryTree()
+    add_values_to_empty_tree(tree_a, [20, 10, 30, 5, 15, 25, 35])
+
+    tree_b = BinaryTree()  # Empty tree
+
+    assert tree_intersection(tree_a, tree_b) == []
+
+def test_one_common_element():
+    tree_a = BinaryTree()
+    add_values_to_empty_tree(tree_a, [40, 20, 60, 10, 30, 50, 70])
+
+    tree_b = BinaryTree()
+    add_values_to_empty_tree(tree_b, [15, 5, 25, 35, 45, 55, 40])
+
+    assert tree_intersection(tree_a, tree_b) == [40]
+
+def test_common_elements_are_leaves():
+    tree_a = BinaryTree()
+    add_values_to_empty_tree(tree_a, [100, 50, 150, 25, 75, 125, 175])
+
+    tree_b = BinaryTree()
+    add_values_to_empty_tree(tree_b, [200, 100, 300, 50, 150, 250, 350])
+
+    assert sorted(tree_intersection(tree_a, tree_b)) == sorted([100, 50, 150])
